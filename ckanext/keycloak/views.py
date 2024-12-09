b6798692a0d1af40c104b652d373755f58f73792
yulrio <33221012@std.stei.itb.ac.id>
2024-12-09 17:21:29 +0700

    Initial Commit

--- ckanext/keycloak/views.py
+++ ckanext/keycloak/views.py
@@ -118,10 +118,11 @@ def sso_logout():
     context['user'] = None
     context['auth_user_obj'] = None
 
-    response = tk.redirect_to(tk.url_for('user.login', context))
+    response = tk.redirect_to(tk.url_for('user.logout', context))
 
     # Redirect ke Keycloak logout URL
-    return tk.redirect_to(f"{logout_uri}")
+    return response
+    # return tk.redirect_to(f"{logout_uri}")
 
 def sso_login_welcome():
     return jsonify({
