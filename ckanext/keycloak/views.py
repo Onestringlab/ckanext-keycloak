02e699873c32b091b11ac65b1dce88b4e684d9b6
yulrio <33221012@std.stei.itb.ac.id>
2024-12-29 20:31:37 +0700

    Initial Commit

--- ckanext/keycloak/views.py
+++ ckanext/keycloak/views.py
@@ -21,7 +21,6 @@ realm_name = tk.config.get('ckanext.keycloak.realm_name', environ.get('CKANEXT__
 redirect_uri = tk.config.get('ckanext.keycloak.redirect_uri', environ.get('CKANEXT__KEYCLOAK__REDIRECT_URI'))
 client_secret_key = tk.config.get('ckanext.keycloak.client_secret_key', environ.get('CKANEXT__KEYCLOAK__CLIENT_SECRET_KEY'))
 logout_uri = tk.config.get('ckanext.keycloak.logout_uri', environ.get('CKANEXT__KEYCLOAK__LOGOUT_URI'))
-domain_url = tk.config.get('ckanext.keycloak.domain_url', environ.get('CKANEXT__KEYCLOAK__DOMAIN_URL'))
 
 client = KeycloakClient(server_url, client_id, realm_name, client_secret_key)
 
@@ -126,13 +125,14 @@ def sso_logout():
     response = tk.redirect_to(f"{logout_uri}")
     response = make_response(response)
 
+    domain_url = tk.config.get('ckanext.keycloak.domain_url', environ.get('CKANEXT__KEYCLOAK__DOMAIN_URL'))
     log.info(f'domain_url: {domain_url}')
     if domain_url == 'http://localhost:5000':
         log.info(f'domain_url: {domain_url}')
         domain_url = ''
 
-    # response.delete_cookie('auth_tkt', path='/', domain=domain_url)
-    # response.delete_cookie('ckan', path='/', domain=domain_url)
+    response.delete_cookie('auth_tkt', path='/', domain=domain_url)
+    response.delete_cookie('ckan', path='/', domain=domain_url)
 
     return response
     # return tk.redirect_to(tk.url_for('user.login'))
