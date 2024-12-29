3eaa58a2e80edf744148029634479f32e503dd8c
yulrio <33221012@std.stei.itb.ac.id>
2024-12-29 19:21:53 +0700

    Initial Commit

--- ckanext/keycloak/views.py
+++ ckanext/keycloak/views.py
@@ -135,7 +135,7 @@ def sso_logout():
 
 def sso_login_welcome():
     return jsonify({
-                "message": "Welcome to SSO 3",
+                "message": "Welcome to SSO 3.1",
                 "success": True
             })
 
