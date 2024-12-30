d858d57aa6e3aacd5c7cc57757bde1b70aec6c57
yulrio <33221012@std.stei.itb.ac.id>
2024-12-30 10:29:00 +0700

    Initial Commit

--- ckanext/keycloak/views.py
+++ ckanext/keycloak/views.py
@@ -131,8 +131,8 @@ def sso_logout():
         response.delete_cookie('auth_tkt', path='/')
     else:
         log.info(f'domain_url: {domain_url}')
-        response.delete_cookie('auth_tkt', path='/', domain=f'.{domain_url}')
-        response.delete_cookie('auth_tkt', path='/', domain=f'{domain_url}')
+        # response.delete_cookie('auth_tkt', path='/', domain=f'.{domain_url}')
+        # response.delete_cookie('auth_tkt', path='/', domain=f'{domain_url}')
 
     response.set_cookie(
         'domain_url',   # Nama cookie
@@ -150,7 +150,7 @@ def sso_logout():
 
 def sso_login_welcome():
     return jsonify({
-                "message": "Welcome to SSO 4.1",
+                "message": "Welcome to SSO 4.2",
                 "success": True
             })
 
