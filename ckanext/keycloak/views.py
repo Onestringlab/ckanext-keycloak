7ac09a43e7bd21a576e80f983e1570efb0789636
yulrio <33221012@std.stei.itb.ac.id>
2024-12-30 13:06:12 +0700

    Initial Commit

--- ckanext/keycloak/views.py
+++ ckanext/keycloak/views.py
@@ -114,7 +114,6 @@ def reset_password():
 def sso_logout():
     log.info("**************** Logout success 4 ********************")
 
-    # Buat respons untuk menghapus cookie dan arahkan ke login
     response = tk.redirect_to(f"{logout_uri}")
     response = make_response(response)
 
@@ -132,13 +131,13 @@ def sso_logout():
         response.delete_cookie('auth_tkt', path='/', domain=f'.{domain_url}')
 
     response.set_cookie(
-        'cookie_value',   # Nama cookie
-        cookie_value,  # Nilai cookie
-        max_age=3600,    # Waktu berlaku dalam detik (opsional)
-        path='/',        # Path cookie (opsional, default '/')
-        secure=True,     # Cookie hanya dikirim melalui HTTPS (opsional)
-        httponly=True,   # Cookie hanya dapat diakses oleh browser, tidak oleh JavaScript (opsional)
-        samesite='Lax'   # SameSite untuk keamanan lintas situs (opsional: 'Lax', 'Strict', atau 'None')
+        'cookie_value',  
+        cookie_value,  
+        max_age=3600,    
+        path='/',        
+        secure=True,    
+        httponly=True,   
+        samesite='Lax'   
     )
 
     return response
