45aab819dc1fd9fa082bdb97cb0820828e6ec9fd
yulrio <33221012@std.stei.itb.ac.id>
2025-01-08 10:45:13 +0700

    Initial Commit

--- ckanext/keycloak/views.py
+++ ckanext/keycloak/views.py
@@ -12,7 +12,7 @@ from ckan.views.user import set_repoze_user, RequestResetView
 from flask import Blueprint,jsonify,make_response,redirect,request
 
 from ckanext.keycloak.utils import get_username, get_profile_by_username
-# from ckanext.keycloak.utils import get_cookie_authorization
+from ckanext.keycloak.utils import get_cookie_authorization
 
 log = logging.getLogger(__name__)
 
