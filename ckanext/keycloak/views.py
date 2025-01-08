49ff6ecc4cffabefe8db33989c6ce2531f03b149
yulrio <33221012@std.stei.itb.ac.id>
2025-01-08 10:08:11 +0700

    Initial Commit

--- ckanext/keycloak/views.py
+++ ckanext/keycloak/views.py
@@ -77,7 +77,7 @@ def sso_check():
         cookies = request.headers.get("Cookie")
         token = str(get_cookie_authorization(cookies))
         if token:
-            if isinstance(token, str) and token.startswith("Bearer "):
+            if token.startswith("Bearer "):
                 return jsonify({"error": "Invalid authorization format"}), 400
             token_value = token.split(" ", 1)[1]
             _, email = get_username(token_value)
