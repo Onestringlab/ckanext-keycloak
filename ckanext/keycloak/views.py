00ba4ac9f6b0579aefcb1dce1d9bccc76b3a679a
yulrio <33221012@std.stei.itb.ac.id>
2025-01-15 16:18:53 +0700

    Initial Commit

--- ckanext/keycloak/views.py
+++ ckanext/keycloak/views.py
@@ -104,6 +104,7 @@ def sso_check():
                 response = tk.redirect_to(tk.url_for('user.me', context))
                 _log_user_into_ckan(response)
                 log.info("Logged in success")
+                response.headers["Access-Control-Allow-Origin"] = "*"
 
                 # response = tk.redirect_to(ckan_url)
                 return response
