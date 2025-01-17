1d70cf349e64f09f43e49fe2e52ddfecb062df10
yulrio <33221012@std.stei.itb.ac.id>
2025-01-17 10:32:37 +0700

    Initial Commit

--- ckanext/keycloak/views.py
+++ ckanext/keycloak/views.py
@@ -127,7 +127,7 @@ def sso_check_post():
             token_value = token.split(" ", 1)[1]
             log.info(f"token_value: {token_value}")
             validated = validate_token(token_value)
-            log.info(f'{validated}')
+            log.info(f'vaaliiiiidd{validated}')
             if not validated:
                 log.info(f'nooottt  {validated}')
                 return jsonify({"error": "Invalid authorization format"}), 400
